47a5abd173dcfdc2853df2f19cc5188d165db422
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2022-07-24 13:44:10 +0530

    Introduced days of the month parser. Added relevant tests.

---
+++ parser/days-of-the-month-parser.go
@@ -0,0 +1,45 @@
+package parser
+
+import (
+	"cron-expression-parser/common/parser"
+	"cron-expression-parser/constant"
+	"cron-expression-parser/parser-interface"
+	"strings"
+)
+
+const (
+	firstDayOfTheMonth = 1
+	lastDayOfTheMonth  = 31
+)
+
+type dayOfTheMonthParser struct {
+	commonParser parser.CommonParser
+}
+
+func NewDayOfTheMonthParser(commonParser parser.CommonParser) parser_interface.Parser {
+	return dayOfTheMonthParser{commonParser: commonParser}
+}
+
+func (m dayOfTheMonthParser) IsValid(daysOfTheMonth string) bool {
+	return true
+}
+
+func (m dayOfTheMonthParser) Parse(daysOfTheMonthComponent string) ([]string, error) {
+	result := make([]string, 0)
+	for _, individualComponent := range strings.Split(daysOfTheMonthComponent, constant.ListValueSeparator) {
+		parsedRes, parseErr := m.commonParser.Parse(individualComponent, m)
+		if parseErr != nil {
+			return nil, parseErr
+		}
+		result = append(result, parsedRes...)
+	}
+	return result, nil
+}
+
+func (m dayOfTheMonthParser) MinAllowedValue() int {
+	return firstDayOfTheMonth
+}
+
+func (m dayOfTheMonthParser) MaxAllowedValue() int {
+	return lastDayOfTheMonth
+}
---
+++ parser/days-of-the-month-parser_test.go
@@ -0,0 +1,77 @@
+package parser
+
+import (
+	"cron-expression-parser/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDaysOfTheMonthParser_Parse_ShouldCallTheCommonParser_Once_AndReturnCombinedValues(t *testing.T) {
+	type scenario struct {
+		name                 string
+		daysOfTheMonthParser string
+		expectedValue        []string
+	}
+	scenarios := []scenario{
+		{
+			name:                 "single daysOfTheMonth",
+			daysOfTheMonthParser: "5",
+			expectedValue:        []string{"5"},
+		},
+		{
+			name:                 "range of daysOfTheMonth",
+			daysOfTheMonthParser: "1-2",
+			expectedValue:        []string{"1", "2"},
+		},
+		{
+			name:                 "range with step",
+			daysOfTheMonthParser: "1-3/1",
+			expectedValue:        []string{"1", "2", "3"},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		ctrl := gomock.NewController(t)
+		mockCommonParser := mocks.NewMockCommonParser(ctrl)
+		mockCommonParser.EXPECT().Parse(scenario.daysOfTheMonthParser, gomock.Any()).Return(scenario.expectedValue, nil)
+
+		dayOfTheMonthParser := NewDayOfTheMonthParser(mockCommonParser)
+		actualValue, err := dayOfTheMonthParser.Parse(scenario.daysOfTheMonthParser)
+
+		assert.Nil(t, err)
+		assert.Equal(t, scenario.expectedValue, actualValue)
+	}
+}
+
+func TestTestDaysOfTheMonthParser_Parse_ShouldCallTheCommonParserThrice_AsThereAreThreeListValues(t *testing.T) {
+	daysOfTheMonth := "3-4,6,7"
+	ctrl := gomock.NewController(t)
+	mockCommonParser := mocks.NewMockCommonParser(ctrl)
+	mockCommonParser.EXPECT().Parse("3-4", gomock.Any()).Return([]string{"3", "4"},nil)
+	mockCommonParser.EXPECT().Parse("6", gomock.Any()).Return([]string{"6"},nil)
+	mockCommonParser.EXPECT().Parse("7", gomock.Any()).Return([]string{"7"},nil)
+
+	daysOfTheMonthParser := NewDayOfTheMonthParser(mockCommonParser)
+	actualValue, err := daysOfTheMonthParser.Parse(daysOfTheMonth)
+
+	expectedValue := []string{"3", "4", "6", "7"}
+	assert.Equal(t, expectedValue, actualValue)
+	assert.Nil(t, err)
+}
+
+func TestTestDaysOfTheMonthParser_MaxAllowedValue_ShouldReturn_31(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	mockCommonParser := mocks.NewMockCommonParser(mockCtrl)
+
+	daysOfTheMonthParser := NewDayOfTheMonthParser(mockCommonParser)
+	assert.Equal(t, 31, daysOfTheMonthParser.MaxAllowedValue())
+}
+
+func TestTestDaysOfTheMonthParser_MinAllowedValue_ShouldReturn_1(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	mockCommonParser := mocks.NewMockCommonParser(mockCtrl)
+
+	daysOfTheMonthParser := NewDayOfTheMonthParser(mockCommonParser)
+	assert.Equal(t, 1, daysOfTheMonthParser.MinAllowedValue())
+}

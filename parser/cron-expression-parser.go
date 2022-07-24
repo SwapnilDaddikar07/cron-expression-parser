611214b77225baeb90899d5dd54bdeec564cc670
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2022-07-24 13:48:00 +0530

    Introduced cron expression parser. This will serve as the entry point to parsing.

---
+++ parser/cron-expression-parser.go
@@ -0,0 +1,94 @@
+package parser
+
+import (
+	"cron-expression-parser/parser-interface"
+	"cron-expression-parser/model"
+	"errors"
+	"strings"
+)
+
+const (
+	separator                            string = " "
+	allowedCronExpressionComponentsCount int    = 5
+)
+
+type CronExpressionParser interface {
+	Parse(expression string) (model.ParsedCronExpression, error)
+	BuildRawCronExpressionComponents(expression string) (model.RawCronExpressionComponents, error)
+}
+
+type cronExpressionParser struct {
+	minuteParser         parser_interface.Parser
+	hourParser           parser_interface.Parser
+	daysOfTheMonthParser parser_interface.Parser
+	monthParser          parser_interface.Parser
+	daysOfTheWeekParser  parser_interface.Parser
+}
+
+func NewCronExpressionParser(minuteParser parser_interface.Parser,
+	hourParser parser_interface.Parser,
+	daysOfTheMonthParser parser_interface.Parser,
+	monthParser parser_interface.Parser,
+	daysOfTheWeekParser parser_interface.Parser) CronExpressionParser {
+	return cronExpressionParser{minuteParser: minuteParser,
+		hourParser:           hourParser,
+		daysOfTheMonthParser: daysOfTheMonthParser,
+		monthParser:          monthParser,
+		daysOfTheWeekParser:  daysOfTheWeekParser}
+}
+
+func (c cronExpressionParser) Parse(expression string) (model.ParsedCronExpression, error) {
+	rawCronExpressionComponents, err := c.BuildRawCronExpressionComponents(expression)
+	if err != nil {
+		return model.ParsedCronExpression{}, err
+	}
+	return c.parse(rawCronExpressionComponents)
+}
+
+func (c cronExpressionParser) BuildRawCronExpressionComponents(expression string) (model.RawCronExpressionComponents, error) {
+	individualComponents := strings.Split(expression, separator)
+	if len(individualComponents) != allowedCronExpressionComponentsCount {
+		return model.RawCronExpressionComponents{}, errors.New("invalid cron expression")
+	}
+	return model.NewRawCronExpressionComponent(individualComponents[0],
+		individualComponents[1],
+		individualComponents[2],
+		individualComponents[3],
+		individualComponents[4]), nil
+}
+
+func (c cronExpressionParser) parse(components model.RawCronExpressionComponents) (parsedExpression model.ParsedCronExpression, err error) {
+	var parsedCronExpression model.ParsedCronExpression
+
+	parsedMinutes, err := c.minuteParser.Parse(components.Minute)
+	if err != nil {
+		return parsedExpression, err
+	}
+	parsedCronExpression.Minutes = parsedMinutes
+
+	parsedHours, err := c.hourParser.Parse(components.Hour)
+	if err != nil {
+		return parsedExpression, err
+	}
+	parsedCronExpression.Hours = parsedHours
+
+	parsedDaysOfTheMonth, err := c.daysOfTheMonthParser.Parse(components.DayOfTheMonth)
+	if err != nil {
+		return parsedExpression, err
+	}
+	parsedCronExpression.DaysOfTheMonth = parsedDaysOfTheMonth
+
+	parsedMonths, err := c.monthParser.Parse(components.Month)
+	if err != nil {
+		return parsedExpression, err
+	}
+	parsedCronExpression.Months = parsedMonths
+
+	parsedDaysOfTheWeek, err := c.daysOfTheWeekParser.Parse(components.DayOfTheWeek)
+	if err != nil {
+		return parsedExpression, err
+	}
+	parsedCronExpression.DaysOfTheWeek = parsedDaysOfTheWeek
+	return parsedCronExpression, nil
+
+}
---
+++ parser/cron-expression-parser_test.go
@@ -0,0 +1,135 @@
+package parser
+
+import (
+	"cron-expression-parser/mocks"
+	"cron-expression-parser/model"
+	"errors"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCronExpressionParser_BuildRawCronExpressionComponents_ShouldParseInputInto_RawCronExpressionComponents(t *testing.T) {
+	type scenario struct {
+		name          string
+		input         string
+		expectedValue model.RawCronExpressionComponents
+	}
+
+	scenarios := []scenario{
+		{
+			name:  "a random valid cron expression with 5 spaces, combination of range and step in days of the month component ",
+			input: "1 2 3-4/2 4 4",
+			expectedValue: model.RawCronExpressionComponents{
+				Minute:        "1",
+				Hour:          "2",
+				DayOfTheMonth: "3-4/2",
+				Month:         "4",
+				DayOfTheWeek:  "4",
+			},
+		},
+		{
+			name:  "a random valid cron expression with 5 spaces, combination of range and step in days of the month component and minute component ",
+			input: "1-2 2 3-4/2 4 4",
+			expectedValue: model.RawCronExpressionComponents{
+				Minute:        "1-2",
+				Hour:          "2",
+				DayOfTheMonth: "3-4/2",
+				Month:         "4",
+				DayOfTheWeek:  "4",
+			},
+		},
+		{
+			name:  "a random valid cron expression with 5 spaces, combination of range and step in days of the month component and minute component and any value identifier",
+			input: "1-2 */2 3-4/2 2 4",
+			expectedValue: model.RawCronExpressionComponents{
+				Minute:        "1-2",
+				Hour:          "*/2",
+				DayOfTheMonth: "3-4/2",
+				Month:         "2",
+				DayOfTheWeek:  "4",
+			},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		ctrl := gomock.NewController(t)
+		minuteParser := mocks.NewMockParser(ctrl)
+		hourParser := mocks.NewMockParser(ctrl)
+		daysOfMonthParser := mocks.NewMockParser(ctrl)
+		monthParser := mocks.NewMockParser(ctrl)
+		daysOfTheWeekParser := mocks.NewMockParser(ctrl)
+
+		cronExpressionParser := NewCronExpressionParser(minuteParser, hourParser, daysOfMonthParser, monthParser, daysOfTheWeekParser)
+		actualValue, err := cronExpressionParser.BuildRawCronExpressionComponents(scenario.input)
+
+		assert.Nil(t, err)
+		assert.Equal(t, scenario.expectedValue, actualValue)
+	}
+}
+
+func TestCronExpressionParser_Parse_CallRelevantParsersAnd_ReturnParsedCronExpression(t *testing.T) {
+	expression := "1-2 3 4-10/2 2 3-5"
+	expectedValue := model.ParsedCronExpression{
+		Minutes:        []string{"1", "2"},
+		Hours:          []string{"3"},
+		DaysOfTheMonth: []string{"4", "6", "8", "10"},
+		Months:         []string{"2"},
+		DaysOfTheWeek:  []string{"3", "4", "5"},
+	}
+
+	ctrl := gomock.NewController(t)
+	minuteParser := mocks.NewMockParser(ctrl)
+	hourParser := mocks.NewMockParser(ctrl)
+	daysOfTheMonthParser := mocks.NewMockParser(ctrl)
+	monthParser := mocks.NewMockParser(ctrl)
+	daysOfTheWeekParser := mocks.NewMockParser(ctrl)
+
+	minuteParser.EXPECT().Parse("1-2").Return([]string{"1", "2"}, nil)
+	hourParser.EXPECT().Parse("3").Return([]string{"3"}, nil)
+	daysOfTheMonthParser.EXPECT().Parse("4-10/2").Return([]string{"4", "6", "8", "10"}, nil)
+	monthParser.EXPECT().Parse("2").Return([]string{"2"}, nil)
+	daysOfTheWeekParser.EXPECT().Parse("3-5").Return([]string{"3", "4", "5"}, nil)
+
+	cronExpressionParser := NewCronExpressionParser(minuteParser, hourParser, daysOfTheMonthParser, monthParser, daysOfTheWeekParser)
+	actualValue, err := cronExpressionParser.Parse(expression)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedValue, actualValue)
+}
+
+func TestCronExpressionParser_Parse_ShouldReturnError_When_ExpressionDoesNotHaveFiveSubSections(t *testing.T) {
+	type scenario struct {
+		name       string
+		expression string
+	}
+	scenarios := []scenario{
+		{
+			name:       "invalid input",
+			expression: "   ",
+		},
+		{
+			name:       "invalid input",
+			expression: " 1 2 3-2",
+		},
+		{
+			name:       "invalid input",
+			expression: "1 2 3-2 7,2,2",
+		},
+	}
+	expectedValue := errors.New("invalid cron expression")
+
+	for _, scenario := range scenarios {
+		ctrl := gomock.NewController(t)
+		minuteParser := mocks.NewMockParser(ctrl)
+		hourParser := mocks.NewMockParser(ctrl)
+		dayOfTheMonthParser := mocks.NewMockParser(ctrl)
+		monthParser := mocks.NewMockParser(ctrl)
+		dayOfTheWeekParser := mocks.NewMockParser(ctrl)
+
+		cronExpressionParser := NewCronExpressionParser(minuteParser, hourParser, dayOfTheMonthParser, monthParser, dayOfTheWeekParser)
+		_, err := cronExpressionParser.Parse(scenario.expression)
+
+		assert.Equal(t, expectedValue, err)
+	}
+}

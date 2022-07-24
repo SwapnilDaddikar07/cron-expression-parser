cac431df087c4b43876127ebbe3185857e662693
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2022-07-24 12:14:34 +0530

    Introduced a parsed cron expression struct. This holds the result of the parsed cron expression.

---
+++ model/parsed-cron-expression-component.go
@@ -0,0 +1,9 @@
+package model
+
+type ParsedCronExpression struct {
+	Minutes        []string
+	Hours          []string
+	DaysOfTheMonth []string
+	Months         []string
+	DaysOfTheWeek  []string
+}
